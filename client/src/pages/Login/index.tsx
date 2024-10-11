1cb27c459b6c8d1c6b0246f57095663c61d2b485
Ronye Vernaes <remnahush@gmail.com>
2024-10-11 19:52:14 +0200

    add login page

---
+++ client/src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import { Layout } from "../../components/layout";
+import { Button } from "../../components/ui";
+
+export const LoginPage: FC = () => {
+  return (
+    <Layout>
+      <Button>Login</Button>
+    </Layout>
+  );
+};
