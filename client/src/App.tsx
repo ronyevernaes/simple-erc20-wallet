0e7a5aee94c242aa48f8ff6d7cd9a4e348eb3ad4
Ronye Vernaes <remnahush@gmail.com>
2024-10-11 19:54:12 +0200

    implement router in app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,8 @@ import { FC } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 
-import { Layout } from "./components/layout";
-import { Button } from "./components/ui";
 import { config } from "./config";
+import { Router } from "./router";
 
 const queryClient = new QueryClient();
 
@@ -12,9 +11,7 @@ const App: FC = () => {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <Layout>
-          <Button>Login</Button>
-        </Layout>
+        <Router />
       </QueryClientProvider>
     </WagmiProvider>
   );
