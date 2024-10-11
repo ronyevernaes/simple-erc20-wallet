c2616e29023781b9d4b67c0302e37194544b706f
Ronye Vernaes <remnahush@gmail.com>
2024-10-11 17:17:27 +0200

    wagmi and tanstack config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,34 @@
 import { FC } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+
+import { config } from "./config";
+
+const queryClient = new QueryClient();
 
 const App: FC = () => {
   return (
-    <div className="flex flex-col justify-between items-center h-screen gap-8 px-0 md:px-8">
-      <header className="flex justify-center md:justify-between py-2 md:py-4 w-full">
-        <h1 className="text-3xl font-bold">
-          SimpleX Wallet
-        </h1>
-      </header>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <div
+          className="flex flex-col justify-between items-center h-screen gap-8 px-0 md:px-8"
+        >
+          <header
+            className="flex justify-center md:justify-between py-2 md:py-4 w-full"
+          >
+            <h1 className="text-3xl font-bold">
+              SimpleX Wallet
+            </h1>
+          </header>
 
-      <button>Login</button>
+          <button>Login</button>
 
-      <footer className="py-2 w-full text-center">
-        {new Date().getFullYear()} &copy; All rights reserved
-      </footer>
-    </div>
+          <footer className="py-2 w-full text-center">
+            {new Date().getFullYear()} &copy; All rights reserved
+          </footer>
+        </div>
+      </QueryClientProvider>
+    </WagmiProvider>
   )
 };
 
