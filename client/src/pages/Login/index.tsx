098cafa173ab4940cac7fe47e4fc3152844d433e
Ronye Vernaes <remnahush@gmail.com>
2024-10-14 06:01:37 +0200

    remove layout from login page

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,24 +1,21 @@
 import { FC } from "react";
 import { useConnect } from "wagmi";
 
-import { Layout } from "../../components/layout";
 import { Button } from "../../components/ui";
 
 export const LoginPage: FC = () => {
   const { connectors, connect } = useConnect();
 
   return (
-    <Layout>
-      <div className="flex flex-col gap-4">
-        {connectors.map((connector) => (
-          <Button
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-          >
-            {connector.name}
-          </Button>
-        ))}
-      </div>
-    </Layout>
+    <div className="flex flex-col gap-4">
+      {connectors.map((connector) => (
+        <Button
+          key={connector.uid}
+          onClick={() => connect({ connector })}
+        >
+          {connector.name}
+        </Button>
+      ))}
+    </div>
   );
 };
